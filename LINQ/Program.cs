49b67d119b6b9b5d16730dd6446f6504ea2eab73
ghosh <ghosharghya918@gmail.com>
2025-11-22 17:59:16 +0530

    select from and where

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int[] age = { 12, 23, 13, 33, 21, 22 };
+var a =from i in age where i>20 orderby i descending select i;
+
+foreach (var item in a)
+{
+    Console.WriteLine(item);
+}
